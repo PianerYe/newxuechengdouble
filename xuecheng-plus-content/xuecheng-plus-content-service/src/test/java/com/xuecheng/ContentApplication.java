a690bccc815f6354dfb0cdc314e2cb97039d259d
yepianer <aimeinvshen@126.com>
2024-01-12 18:12:40 +0800

    课程查询-DAO接口

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/ContentApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng;
+/**
+ * @author yepianer
+ * @date 2024/1/12 13:56
+ * @project_name xuechengnew
+ * @description 内容管理服务启动类
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class,args);
+    }
+}
