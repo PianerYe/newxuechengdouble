0bd2030740b7df0edcad433474cc9ca28f55b196
yepianer <aimeinvshen@126.com>
2024-01-21 23:45:39 +0800

    nacos配置-配置content以及system在nacos上

--- xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/DictionaryController.java
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/DictionaryController.java
@@ -2,6 +2,8 @@ package com.xuecheng.system.controller;
 
 import com.xuecheng.system.model.po.Dictionary;
 import com.xuecheng.system.service.DictionaryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,16 +21,19 @@ import java.util.List;
  */
 @Slf4j
 @RestController
+@Api(value = "系统管理信息接口",tags = "系统管理信息接口")
 public class DictionaryController  {
 
     @Autowired
     private DictionaryService  dictionaryService;
 
+    @ApiOperation("查询所有数据字典信息")
     @GetMapping("/dictionary/all")
     public List<Dictionary> queryAll() {
         return dictionaryService.queryAll();
     }
 
+    @ApiOperation("按照代码查询数据字典信息")
     @GetMapping("/dictionary/code/{code}")
     public Dictionary getByCode(@PathVariable String code) {
         return dictionaryService.getByCode(code);
