2150d737ef894ec336e41a2f2fac935c3b2fc579
yepianer <aimeinvshen@126.com>
2024-03-11 10:26:08 +0800

    课程发布-接口开发1

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CoursePublishController.java
@@ -0,0 +1,36 @@
+package com.xuecheng.content.api;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author yepianer
+ * @date 2023/9/10 0:42
+ * @project_name yepianerxuecheng
+ * @description
+ */
+@Api(value = "课程预览发布接口",tags = "课程预览发布接口")
+@Controller
+public class CoursePublishController {
+
+//    @Resource
+//    CoursePublishService coursePublishService;
+    /**
+     * 模版接口
+     * */
+    @GetMapping("/coursepreview/{courseId}")
+    public ModelAndView preview(@PathVariable("courseId") long courseId){
+        ModelAndView modelAndView = new ModelAndView();
+        //查询课程的信息作为模型数据
+//        CoursePreviewDto coursePreviewInfo = coursePublishService.getCoursePreviewInfo(courseId);
+        //指定模型
+//        modelAndView.addObject("model",coursePreviewInfo);
+        //指定模板
+        modelAndView.setViewName("course_template");//根据视图名称加.ftl找到模板
+        return modelAndView;
+    }
+
+}
