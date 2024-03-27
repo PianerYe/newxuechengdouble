c0b6eab46fb8f7eb8a16424868b3ce2c8c95be3d
yepianer <aimeinvshen@126.com>
2024-03-28 07:44:32 +0800

    课程搜索完结以及测试

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/jobhandler/CoursePublishTask.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/jobhandler/CoursePublishTask.java
@@ -32,8 +32,8 @@ public class CoursePublishTask extends MessageProcessAbstract {
 
     @Resource
     CoursePublishService coursePublishService;
-//    @Resource
-//    SearchServiceClinet searchServiceClinet;
+    @Resource
+    SearchServiceClinet searchServiceClinet;
     @Resource
     CoursePublishMapper coursePublishMapper;
     //coursePublishJobHandler
@@ -55,9 +55,9 @@ public class CoursePublishTask extends MessageProcessAbstract {
         //课程静态化上传到minio
         generateCourseHtml(mqMessage,courseId);
         //向elasticsearch写索引数据
-//        saveCourseIndex(mqMessage,courseId);
+        saveCourseIndex(mqMessage,courseId);
         //向redis写缓存
-//        saveCourseCache(mqMessage,courseId);
+        saveCourseCache(mqMessage,courseId);
         //返回true表示任务完成
         return true;
     }
@@ -87,46 +87,46 @@ public class CoursePublishTask extends MessageProcessAbstract {
     }
 
     //保存课程索引信息，第二个阶段任务
-//    private void saveCourseIndex(MqMessage mqMessage,Long courseId){
-//        //任务id
-//        Long taskId = mqMessage.getId();
-//        MqMessageService mqMessageService = this.getMqMessageService();
-//        //取出第二个阶段状态
-//        int stageTwo = mqMessageService.getStageTwo(taskId);
-//        //做任务幂等性处理
-//        if (stageTwo>0){
-//            log.debug("课程索引信息已写入，无需执行...");
-//            return;
-//        }
-//        //查询课程信息，调用搜索服务添加索引接口
-//        //从课程发布表查询课程信息
-//        CoursePublish coursePublish = coursePublishMapper.selectById(courseId);
-//
-//        CourseIndex courseIndex = new CourseIndex();
-//        BeanUtils.copyProperties(coursePublish,courseIndex);
-//        //远程调用
-//        Boolean add = searchServiceClinet.add(courseIndex);
-//        if (!add){
-//            XueChengPlusException.cast("远程调用搜索服务，添加索引失败");
-//        }
-//        //。。任务处理完成写任务状态为完成
-//        mqMessageService.completedStageTwo(taskId);
-//    }
+    private void saveCourseIndex(MqMessage mqMessage,Long courseId){
+        //任务id
+        Long taskId = mqMessage.getId();
+        MqMessageService mqMessageService = this.getMqMessageService();
+        //取出第二个阶段状态
+        int stageTwo = mqMessageService.getStageTwo(taskId);
+        //做任务幂等性处理
+        if (stageTwo>0){
+            log.debug("课程索引信息已写入，无需执行...");
+            return;
+        }
+        //查询课程信息，调用搜索服务添加索引接口
+        //从课程发布表查询课程信息
+        CoursePublish coursePublish = coursePublishMapper.selectById(courseId);
 
-//    private void saveCourseCache(MqMessage mqMessage,Long courseId){
-//        //任务id
-//        Long taskId = mqMessage.getId();
-//        MqMessageService mqMessageService = this.getMqMessageService();
-//        //取出第二个阶段状态
-//        int stageThree = mqMessageService.getStageThree(taskId);
-//        //做任务幂等性处理
-//        if (stageThree>0){
-//            log.debug("将课程信息缓存至redis,课程id:{}",courseId);
-//            return;
-//        }
-//        //将课程信息缓存至redis...
-//
-//        //。。任务处理完成写任务状态为完成
-//        mqMessageService.completedStageThree(taskId);
-//    }
+        CourseIndex courseIndex = new CourseIndex();
+        BeanUtils.copyProperties(coursePublish,courseIndex);
+        //远程调用
+        Boolean add = searchServiceClinet.add(courseIndex);
+        if (!add){
+            XueChengPlusException.cast("远程调用搜索服务，添加索引失败");
+        }
+        //。。任务处理完成写任务状态为完成
+        mqMessageService.completedStageTwo(taskId);
+    }
+
+    private void saveCourseCache(MqMessage mqMessage,Long courseId){
+        //任务id
+        Long taskId = mqMessage.getId();
+        MqMessageService mqMessageService = this.getMqMessageService();
+        //取出第二个阶段状态
+        int stageThree = mqMessageService.getStageThree(taskId);
+        //做任务幂等性处理
+        if (stageThree>0){
+            log.debug("将课程信息缓存至redis,课程id:{}",courseId);
+            return;
+        }
+        //将课程信息缓存至redis...
+
+        //。。任务处理完成写任务状态为完成
+        mqMessageService.completedStageThree(taskId);
+    }
 }
